312f7a1c4c68edff0b4389647e3ad0909cb4d406
h4ckitt <dammysteve10@gmail.com>
2023-03-22 15:23:43 +0100

    [change] Start Necessary Service For App Run

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"pirecorder/app"
+	"pirecorder/config"
+	"pirecorder/logger"
+	"pirecorder/web/controller"
+	"pirecorder/web/router"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello, playground")
+	config.Load()
+	logfile := fmt.Sprintf("pirecorder_logs_%s.log", time.Now().Format("2006-01-02_15:04:05"))
+
+	logman, err := logger.NewLogger(fmt.Sprintf("%s/%s", config.GetConfig().LogFolder, logfile))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	svc, err := app.NewApp(logman)
+
+	if err != nil {
+		logman.LogError(err, "Error creating app")
+	}
+
+	ctrl := controller.NewController(svc, logman)
+	r := router.InitRouter(ctrl, logman)
+
+	svc.Start()
+
+	logman.LogInfo("Starting server on port 8080")
+
+	if err = http.ListenAndServe(fmt.Sprintf(":%s", config.GetConfig().Port), r); err != nil {
+		logman.LogError(err, "Error starting server")
+	}
 }
