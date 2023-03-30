b8a353b194bab18f3c3820a230a1a1e3276469be
h4ckitt <dammysteve10@gmail.com>
2023-03-30 15:15:15 +0100

    [add] Switched To Custom Server With TLS Config

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"pirecorder/app"
 	"pirecorder/config"
 	"pirecorder/logger"
@@ -33,7 +35,35 @@ func main() {
 
 	logman.LogInfo("Starting server on port 8080")
 
-	if err = http.ListenAndServe(fmt.Sprintf(":%s", config.GetConfig().Port), r); err != nil {
-		logman.LogError(err, "Error starting server")
+	sslConfig := config.GetConfig().SSLConfig
+
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%s", config.GetConfig().Port),
+		Handler: r,
+	}
+	var useSSL bool
+
+	if sslConfig.CertFile != "" && sslConfig.KeyFile != "" {
+		if _, err = os.Stat(sslConfig.CertFile); os.IsNotExist(err) {
+			logman.LogError(err, "SSL Cert file not found")
+		} else if _, err = os.Stat(sslConfig.KeyFile); os.IsNotExist(err) {
+			logman.LogError(err, "SSL Key file not found")
+		} else {
+			cert, _ := tls.LoadX509KeyPair(sslConfig.CertFile, sslConfig.KeyFile)
+			server.TLSConfig = &tls.Config{Certificates: []tls.Certificate{cert}}
+			useSSL = true
+		}
+	}
+
+	if useSSL {
+		logman.LogInfo(fmt.Sprintf("Starting server on port %s with SSL", config.GetConfig().Port))
+		if err = server.ListenAndServeTLS("", ""); err != nil {
+			logman.LogError(err, "Error starting server")
+		}
+	} else {
+		logman.LogInfo(fmt.Sprintf("Starting server on port %s without SSL", config.GetConfig().Port))
+		if err = server.ListenAndServe(); err != nil {
+			logman.LogError(err, "Error starting server")
+		}
 	}
 }
